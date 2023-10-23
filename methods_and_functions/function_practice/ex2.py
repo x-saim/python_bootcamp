e15351218515cc185c7afd1d2b06ff191a8b82bc
Saim Amir <saimxamir@gmail.com>
2023-10-23 15:52:42 -0400

    implement ex2: animal_crackers

---
+++ methods_and_functions/function_practice/ex2.py
@@ -0,0 +1,15 @@
+'''
+Animal Crackers:
+
+A function that takes a two-word string and returns True if both words begin with the same letter.
+
+'''
+
+def animal_crackers(text:str) -> bool:
+  split_text = text.split(' ')
+  
+  return split_text[0][0] != split_text[1][0]
+
+
+print(animal_crackers('Levelheaded Llama'))
+print(animal_crackers('Crazy Kangaroo'))
